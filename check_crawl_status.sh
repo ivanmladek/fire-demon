8691eed0575e5bfe81b13957266edbf0d288725d
PAVEL MACHALEK <pavel@machalek.io>
2025-04-18 14:48:04 +0200

    Stop and cancel all crawls: f70dfe26, 0eed374e, 52f84c2b

--- check_crawl_status.sh
+++ check_crawl_status.sh
@@ -2,13 +2,12 @@
 
 # Check if job ID is provided
 if [ -z "$1" ]; then
-    echo "Error: Please provide a job ID"
     echo "Usage: $0 <job_id>"
     exit 1
 fi
 
-# Store the job ID
-JOB_ID="$1"
+JOB_ID=$1
 
-# Make the curl request and process with jq
-curl -X GET "http://localhost:3002/v0/crawl/status/${JOB_ID}" | jq -r '.data[] | [.metadata.url, .metadata.pageStatusCode] | @csv' 
+# Make the API call and format output as CSV
+curl -X GET "http://localhost:3002/v0/crawl/status/$JOB_ID" | \
+jq -r '.partial_data[] | select(.metadata != null) | [.metadata.url, .metadata.pageStatusCode] | @csv' 
---
+++ test_proxy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Test proxy connection
+curl -v -x http://nodemaven_nospam_simplelogin_com-country-us-filter-medium:ktonj4oufu@gate.nodemaven.com:8080 https://contactout.com/don-burke-36416 
